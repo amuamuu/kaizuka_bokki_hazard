6bf8ed7ea722dd09217e6f098c01cafac66d68d6
amuamuu <stayfreeyour.misery.d.o.d@gmail.com>
2017-08-20 18:36:39 +0900

    ご尊顔追加

--- kaizuka_rate.py
+++ kaizuka_rate.py
@@ -1,3 +1,21 @@
+#　　　　　　　　　　　　　　　,,jlililililililili㎜㎜、
+#　　　　　　　　　　　　　,,,jililililliililililililﾐ
+#　　　　　　　　　　　　 ,,jililil'''"´￣￣￣｀ﾞﾞヾililiﾐ
+#　　　　　　　　　　　 ,jiliｌ!"　　　　　　　　 　 ｀ヾiｌ!
+#　　　　　　　　　　　jilil''　.,.;:;:;:;:;:　　　 :;:;:;:;:;.,.,
+#　　　　　　 　 　 　 jlｉ!　 ''´　　　　　　　　　｀''　 lliｌi
+#　　　　　　 　 　 　 ilｉ!　　 <●>　j　 !　<●>　　liliｌ!　　【
+#　　　　　　　　　　　lili　　　　 　/　　ヽ 　　　 　 lili′　
+#　　　　　　　　　　　 }ｉ!　　　　　!ｏ　 ｏ !　　　　 lil{
+#　　　　　　　　　 　 i´r!　　　　 . : : : : : .　 　 　 h｀i
+#　　　　　　　　　　　〉》l　　 　 . :, ' ｀´ヽ: .　　　 l《〈
+#　　　　　　　　　　　辷j 　 　 　´￣＾￣｀　　 　匕′
+#　　　　　　　　　　　 ｀ヾ、　 、　　　　　　 ,　　/l
+#　　　　　　　　　　 　 　 ヽ　　 　 　 　 　 　 , ′!
+#　　　　　　　　　　　　　　l ＼　　　　　　 ／　　 l
+#　　　　　　　　 　 　 　 　 l　　｀ー--一´　　　　 lー､
+#　 　 　 　 　 　 　 　 　 　 l　　　　　　　　　　　 ﾉ　　ヽー-､＿＿
+
 kaizuka_rate = 35
 kaizuka_price = kaizuka_rate * 512810
 print( str(kaizuka_price) + '貝塚円だで')
