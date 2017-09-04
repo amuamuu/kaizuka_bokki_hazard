f8f851423e7c7fe7e6eed56678dafa5bac12e4b4
amuamuu <stayfreeyour.misery.d.o.d@gmail.com>
2017-09-04 22:27:59 +0900

    入力機能追加

--- kaizuka_rate.py
+++ kaizuka_rate.py
@@ -1,27 +1,28 @@
-#　　　　　　　　　　　　　　　,,jlililililililili㎜㎜、
-#　　　　　　　　　　　　　,,,jililililliililililililﾐ
-#　　　　　　　　　　　　 ,,jililil'''"´￣￣￣｀ﾞﾞヾililiﾐ
-#　　　　　　　　　　　 ,jiliｌ!"　　　　　　　　 　 ｀ヾiｌ!
-#　　　　　　　　　　　jilil''　.,.;:;:;:;:;:　　　 :;:;:;:;:;.,.,
-#　　　　　　 　 　 　 jlｉ!　 ''´　　　　　　　　　｀''　 lliｌi
-#　　　　　　 　 　 　 ilｉ!　　 <●>　j　 !　<●>　　liliｌ!　　【
-#　　　　　　　　　　　lili　　　　 　/　　ヽ 　　　 　 lili′　
-#　　　　　　　　　　　 }ｉ!　　　　　!ｏ　 ｏ !　　　　 lil{
-#　　　　　　　　　 　 i´r!　　　　 . : : : : : .　 　 　 h｀i
-#　　　　　　　　　　　〉》l　　 　 . :, ' ｀´ヽ: .　　　 l《〈
-#　　　　　　　　　　　辷j 　 　 　´￣＾￣｀　　 　匕′
-#　　　　　　　　　　　 ｀ヾ、　 、　　　　　　 ,　　/l
-#　　　　　　　　　　 　 　 ヽ　　 　 　 　 　 　 , ′!
-#　　　　　　　　　　　　　　l ＼　　　　　　 ／　　 l
-#　　　　　　　　 　 　 　 　 l　　｀ー--一´　　　　 lー､
-#　 　 　 　 　 　 　 　 　 　 l　　　　　　　　　　　 ﾉ　　ヽー-､＿＿
+# 　　　　　　　　　　　　　　　,,jlililililililili㎜㎜、
+# 　　　　　　　　　　　　　,,,jililililliililililililﾐ
+# 　　　　　　　　　　　　 ,,jililil'''"´￣￣￣｀ﾞﾞヾililiﾐ
+# 　　　　　　　　　　　 ,jiliｌ!"　　　　　　　　 　 ｀ヾiｌ!
+# 　　　　　　　　　　　jilil''　.,.;:;:;:;:;:　　　 :;:;:;:;:;.,.,
+# 　　　　　　 　 　 　 jlｉ!　 ''´　　　　　　　　　｀''　 lliｌi
+# 　　　　　　 　 　 　 ilｉ!　　 <●>　j　 !　<●>　　liliｌ!　　【
+# 　　　　　　　　　　　lili　　　　 　/　　ヽ 　　　 　 lili′　
+# 　　　　　　　　　　 }ｉ!　　　　　!ｏ　 ｏ !　　　　 lil{
+# 　　　　　　　　　 　 i´r!　　　　 . : : : : : .　 　 　 h｀i
+# 　　　　　　　　　　　〉》l　　 　 . :, ' ｀´ヽ: .　　　 l《〈
+# 　　　　　　　　　　　辷j 　 　 　´￣＾￣｀　　 　匕′
+# 　　　　　　　　　　　 ｀ヾ、　 、　　　　　　 ,　　/l
+# 　　　　　　　　　　 　 　 ヽ　　 　 　 　 　 　 , ′!
+# 　　　　　　　　　　　　　　l ＼　　　　　　 ／　　 l
+# 　　　　　　　　 　 　 　 　 l　　｀ー--一´　　　　 lー､
+# 　 　 　 　 　 　 　 　 　 　 l　　　　　　　　　　　 ﾉ　　ヽー-､＿＿
 
-#日本円
-yen = 512810
+# 日本円
+print('日本円を入力')
+yen = input()
 
-#貝塚レート
+# 貝塚レート
 kaizuka_rate = 35
 
-#シャムの予想される月収1万円と、同世代の平均月収35万円から算出されたシャムの体感する物価
-kaizuka_yen = kaizuka_rate * yen
-print("{:,d}".format(kaizuka_yen) + '貝塚円だで')
+# シャムの予想される月収1万円と、同世代の平均月収35万円から算出されたシャムの体感する物価
+kaizuka_yen = int(kaizuka_rate) * int(yen)
+print(str(kaizuka_yen) + '貝塚円だで')
